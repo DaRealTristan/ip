d4048b5dcf8915c45540deaaa607bacbabf438a4
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-21 11:36:16 +0800

    Refactored if else to a switch

--- src/main/java/Nubish.java
+++ src/main/java/Nubish.java
@@ -15,30 +15,34 @@ public class Nubish {
             """;
         System.out.println(logo + "Hello! I'm Nubish.\nWhat can I do for you?");
 
+        programmeLoop:
         while (true) {
             String input = scanner.nextLine();
             String response = """
-                        _________________________________________________
-                        Nubish: %s
-                        _________________________________________________
-                        """;
+                    _________________________________________________
+                    Nubish: %s
+                    _________________________________________________
+                    """;
 
-            if (input.equals("bye")) {
-                System.out.printf(response, "Bye. Hope to see you again soon!");
-                break;
-            } else if (input.equals("list")) {
-                int count = 0;
-                StringBuilder list = new StringBuilder("\n");
+            switch (input) {
+                case "bye":
+                    System.out.printf(response, "Bye. Hope to see you again soon!");
+                    break programmeLoop;
+                case "list":
+                    int count = 0;
+                    StringBuilder list = new StringBuilder("\n");
 
-                for (int i = 0; i < numOfTasks; i++) {
-                    list.append(String.format("%d. %s\n", i + 1, tasks[i]));
-                }
+                    for (int i = 0; i < numOfTasks; i++) {
+                        list.append(String.format("%d. %s\n", i + 1, tasks[i]));
+                    }
 
-                System.out.printf(response, list);
-            } else {
-                System.out.printf(response, String.format("task added: %s", input));
-                tasks[numOfTasks] = input;
-                numOfTasks++;
+                    System.out.printf(response, list);
+                    break;
+                default:
+                    System.out.printf(response, String.format("task added: %s", input));
+                    tasks[numOfTasks] = input;
+                    numOfTasks++;
+                    break;
             }
         }
     }
