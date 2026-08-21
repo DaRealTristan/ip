d1edbb62485be207ce55d4adf77705550a9a7414
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-21 11:52:50 +0800

    Added Todos, Events and Deadlines

--- src/main/java/Nubish.java
+++ src/main/java/Nubish.java
@@ -1,5 +1,8 @@
 import java.util.Scanner;
 import utils.Task;
+import utils.Deadline;
+import utils.Event;
+import utils.Todo;
 
 public class Nubish {
     public static void main(String[] args) {
@@ -18,14 +21,18 @@ public class Nubish {
 
         programmeLoop:
         while (true) {
-            String input = scanner.nextLine();
+            String input = scanner.nextLine().trim();
+            String[] parts = input.split(" ", 2);
+            String command = parts[0];
+            String arguments = (parts.length > 1) ? parts[1] : "";
+
             String response = """
                     _________________________________________________
                     Nubish: %s
                     _________________________________________________
                     """;
 
-            switch (input) {
+            switch (command) {
                 case "bye":
                     System.out.printf(response, "Bye. Hope to see you again soon!");
                     break programmeLoop;
@@ -40,8 +47,8 @@ public class Nubish {
 
                     System.out.printf(response, list);
                     break;
-                case String s when s.startsWith("mark "):
-                    int indexMark = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                case "mark":
+                    int indexMark = Integer.parseInt(arguments.trim()) - 1;
                     Task tMark = tasks[indexMark];
                     tMark.markAsDone();
                     String replyMark = String.format("""
@@ -50,19 +57,50 @@ public class Nubish {
                             """, tMark.toString());
                     System.out.printf(response, replyMark);
                     break;
-                case String s when s.startsWith("unmark "):
-                    int indexUnmark = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                case "unmark":
+                    int indexUnmark = Integer.parseInt(arguments.trim()) - 1;
                     Task tUnmark = tasks[indexUnmark];
                     tUnmark.unmarkAsDone();
                     String replyUnmark = String.format("""
-                            Nice! I've marked this task as done:
+                            I've unmarked this task:
                                 %s
                             """, tUnmark.toString());
                     System.out.printf(response, replyUnmark);
                     break;
+                case "deadline":
+                    int byIndex = arguments.indexOf("/by");
+                    String taskName = arguments.substring(0, byIndex).trim();
+                    String deadline = arguments.substring(byIndex + 3).trim();
+                    String replyDeadline = String.format("""
+                                Added task: %s (by: %s)
+                            Now you have %d tasks in the list
+                            """, taskName, deadline, numOfTasks + 1);
+                    System.out.printf(response, replyDeadline);
+                    tasks[numOfTasks] = new Deadline(taskName, deadline);
+                    numOfTasks++;
+                    break;
+                case "event":
+                    int fromIndex = arguments.indexOf("/from");
+                    int toIndex = arguments.indexOf("/to");
+
+                    String eventName = arguments.substring(0, fromIndex).trim();
+                    String fromTime = arguments.substring(fromIndex + 5, toIndex).trim();
+                    String toTime = arguments.substring(toIndex + 3).trim();
+
+                    String replyEvent = String.format("""
+                                Added event: %s (From: %s, To: %s)
+                            Now you have %d tasks in the list
+                            """, eventName, fromTime, toTime, numOfTasks + 1);
+                    System.out.printf(response, replyEvent);
+                    tasks[numOfTasks] = new Event(eventName, fromTime, toTime);
+                    numOfTasks++;
+                    break;
                 default:
-                    System.out.printf(response, String.format("task added: %s", input));
-                    tasks[numOfTasks] = new Task(input);
+                    System.out.printf(response, String.format("""
+                            task added: %s
+                        Now you have %d tasks in the list.
+                        """, input, numOfTasks + 1));
+                    tasks[numOfTasks] = new Todo(input);
                     numOfTasks++;
                     break;
             }
