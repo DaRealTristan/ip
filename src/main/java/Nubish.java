b13339d21ad707aa69ace333d1a9bb803a418090
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-21 11:29:16 +0800

    Renamed main class

---
+++ src/main/java/Nubish.java
@@ -0,0 +1,12 @@
+public class Nubish {
+    public static void main(String[] args) {
+        String logo = """
+              _   _ _   _ ____ ___ ____  _   _ 
+             | \\ | | | | | __ )_ _/ ___|| | | |
+             |  \\| | | | |  _ \\| |\\___ \\| |_| |
+             | |\\  | |_| | |_) | | ___) |  _  |
+             |_| \\_|\\___/|____/___|____/|_| |_|
+            """;
+        System.out.println(logo + "Hello! I'm Nubish.\nWhat can I do for you?");
+    }
+}
