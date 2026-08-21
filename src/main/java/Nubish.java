009ca75028023af5075e5c6a44aaa1d1eb7ad0c2
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-21 11:48:15 +0800

    Added marking and unmarking

--- src/main/java/Nubish.java
+++ src/main/java/Nubish.java
@@ -1,9 +1,10 @@
 import java.util.Scanner;
+import utils.Task;
 
 public class Nubish {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int numOfTasks = 0;
 
         String logo = """
@@ -33,14 +34,35 @@ public class Nubish {
                     StringBuilder list = new StringBuilder("\n");
 
                     for (int i = 0; i < numOfTasks; i++) {
-                        list.append(String.format("%d. %s\n", i + 1, tasks[i]));
+                        Task t = tasks[i];
+                        list.append(String.format("%d. %s\n", i + 1, t.toString()));
                     }
 
                     System.out.printf(response, list);
                     break;
+                case String s when s.startsWith("mark "):
+                    int indexMark = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                    Task tMark = tasks[indexMark];
+                    tMark.markAsDone();
+                    String replyMark = String.format("""
+                            Nice! I've marked this task as done:
+                                %s
+                            """, tMark.toString());
+                    System.out.printf(response, replyMark);
+                    break;
+                case String s when s.startsWith("unmark "):
+                    int indexUnmark = Character.getNumericValue(input.charAt(input.length() - 1)) - 1;
+                    Task tUnmark = tasks[indexUnmark];
+                    tUnmark.unmarkAsDone();
+                    String replyUnmark = String.format("""
+                            Nice! I've marked this task as done:
+                                %s
+                            """, tUnmark.toString());
+                    System.out.printf(response, replyUnmark);
+                    break;
                 default:
                     System.out.printf(response, String.format("task added: %s", input));
-                    tasks[numOfTasks] = input;
+                    tasks[numOfTasks] = new Task(input);
                     numOfTasks++;
                     break;
             }
---
+++ src/main/java/utils/Task.java
@@ -0,0 +1,32 @@
+package utils;
+
+public class Task {
+    private String description;
+    private boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "X" : " ");
+    }
+
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    public void unmarkAsDone() {
+        this.isDone = false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s] %s", this.getStatusIcon(), this.description);
+    }
+}
