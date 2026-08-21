651224d2921b682be6dc388ee0fc390a588b0b89
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-21 11:30:50 +0800

    Added echo

--- src/main/java/Nubish.java
+++ src/main/java/Nubish.java
@@ -1,12 +1,36 @@
+import java.util.Scanner;
+
 public class Nubish {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
         String logo = """
               _   _ _   _ ____ ___ ____  _   _ 
              | \\ | | | | | __ )_ _/ ___|| | | |
-             |  \\| | | | |  _ \\| |\\___ \\| |_| |
-             | |\\  | |_| | |_) | | ___) |  _  |
+@@ -8,5 +12,26 @@ public static void main(String[] args) {
              |_| \\_|\\___/|____/___|____/|_| |_|
             """;
         System.out.println(logo + "Hello! I'm Nubish.\nWhat can I do for you?");
+
+        while (true) {
+            String input = scanner.nextLine();
+
+            if (input.equals("bye")) {
+                String response = """
+                        _________________________________________________
+                        Nubish: Bye. Hope to see you again soon!
+                        _________________________________________________
+                        """;
+                System.out.println(response);
+                break;
+            } else {
+                String response = """
+                        _________________________________________________
+                        Nubish: %s
+                        _________________________________________________
+                        """;
+                System.out.printf(response, input);
+            }
+        }
     }
 }
