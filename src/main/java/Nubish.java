3495b2356176a87e601efccac534f052b706e470
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-21 11:33:13 +0800

    Added add and list

--- src/main/java/Nubish.java
+++ src/main/java/Nubish.java
@@ -3,33 +3,42 @@ import java.util.Scanner;
 public class Nubish {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String[] tasks = new String[100];
+        int numOfTasks = 0;
 
         String logo = """
               _   _ _   _ ____ ___ ____  _   _ 
              | \\ | | | | | __ )_ _/ ___|| | | |
-@@ -8,5 +12,26 @@ public static void main(String[] args) {
+             |  \\| | | | |  _ \\| |\\___ \\| |_| |
+             | |\\  | |_| | |_) | | ___) |  _  |
              |_| \\_|\\___/|____/___|____/|_| |_|
             """;
         System.out.println(logo + "Hello! I'm Nubish.\nWhat can I do for you?");
 
         while (true) {
             String input = scanner.nextLine();
-
-            if (input.equals("bye")) {
-                String response = """
-                        _________________________________________________
-                        Nubish: Bye. Hope to see you again soon!
-                        _________________________________________________
-                        """;
-                System.out.println(response);
-                break;
-            } else {
-                String response = """
+            String response = """
                         _________________________________________________
                         Nubish: %s
                         _________________________________________________
                         """;
-                System.out.printf(response, input);
+
+            if (input.equals("bye")) {
+                System.out.printf(response, "Bye. Hope to see you again soon!");
+                break;
+            } else if (input.equals("list")) {
+                int count = 0;
+                StringBuilder list = new StringBuilder("\n");
+
+                for (int i = 0; i < numOfTasks; i++) {
+                    list.append(String.format("%d. %s\n", i + 1, tasks[i]));
+                }
+
+                System.out.printf(response, list);
+            } else {
+                System.out.printf(response, String.format("task added: %s", input));
+                tasks[numOfTasks] = input;
+                numOfTasks++;
             }
         }
     }
