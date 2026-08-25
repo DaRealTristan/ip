84a6ba931d43cf33e005925727fa640008a25289
DaRealTristan <tristan.tay.yu.hng@gmail.com>
2026-08-25 20:26:02 +0800

    Add Date Time feature

--- src/main/java/utils/Deadline.java
+++ src/main/java/utils/Deadline.java
@@ -1,25 +1,35 @@
 package utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Deadline extends Task{
-    private String deadline;
+    private LocalDateTime deadline;
+    private DateTimeFormatter saveFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    private DateTimeFormatter printFormat = DateTimeFormatter.ofPattern("hhmm MMM d yyyy");
 
     public Deadline(String description, String deadline) {
         super(description);
-        this.deadline = deadline;
+        this.deadline = convertToDateTime(deadline);
     }
 
     public Deadline(boolean isDone, String description, String deadline) {
         super(isDone, description);
-        this.deadline = deadline;
+        this.deadline = convertToDateTime(deadline);
+    }
+
+    public LocalDateTime convertToDateTime(String dateTimeString) {
+        System.out.println(dateTimeString);
+        return LocalDateTime.parse(dateTimeString, saveFormat);
     }
 
     @Override
     public String toString() {
-        return String.format("[D]%s (by: %s)", super.toString(), deadline);
+        return String.format("[D]%s (by: %s)", super.toString(), this.deadline.format(printFormat));
     }
 
     @Override
     public String saveString() {
-        return String.format("D | %s | %s", super.saveString(), this.deadline);
+        return String.format("D | %s | %s", super.saveString(), this.deadline.format(saveFormat));
     }
 }
--- src/main/java/utils/Event.java
+++ src/main/java/utils/Event.java
@@ -1,27 +1,38 @@
 package utils;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Event extends Task{
-    private String from;
-    private String to;
+    private LocalDateTime from;
+    private LocalDateTime to;
+    private DateTimeFormatter saveFormat = DateTimeFormatter.ofPattern("dd/MM/yyyy HHmm");
+    private DateTimeFormatter printFormat = DateTimeFormatter.ofPattern("hhmm MMM d yyyy");
 
     public Event(String description, String from, String to) {
         super(description);
-        this.from = from;
-        this.to = to;
+        this.from = convertToDateTime(from);
+        this.to = convertToDateTime(from);
     }
 
     public Event(boolean isDone, String description, String from, String to) {
         super(isDone, description);
-        this.from = from;
-        this.to = to;
+        this.from = convertToDateTime(from);
+        this.to = convertToDateTime(from);
+    }
+
+    public LocalDateTime convertToDateTime(String dateTimeString) {
+        return LocalDateTime.parse(dateTimeString, saveFormat);
     }
 
     @Override
     public String toString() {
-        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from, this.to);
+        return String.format("[E]%s (from: %s to: %s)", super.toString(), this.from.format(printFormat),
+                this.to.format(printFormat));
     }
 
     public String saveString() {
-        return String.format("E | %s | %s | %s", super.saveString(), this.from, this.to);
+        return String.format("E | %s | %s | %s", super.saveString(), this.from.format(saveFormat),
+                this.to.format(saveFormat));
     }
 }
